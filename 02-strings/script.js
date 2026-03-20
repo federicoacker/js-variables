9fe953441455da1d7f560bdbd8440dd413a254b9
federicoacker <acker.federico@gmail.com>
2026-03-20 13:45:04 +0100

    Fatto Esercizio stringhe

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -2,10 +2,22 @@
 // Crea una variabile chiamata name e assegnale il tuo nome.
 // Stampa il valore di nome nella console.
 
+// Chiamata myName e non name perché name sebbene non sia una reserved word per javascript è comunque una data property specifica che è meglio evitare di usare come nome di variabile
+const myName = "Federico"; 
+console.log(myName);
+
 // 2. Tipi primitivi
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
+
+//Stesso concetto, cambiato i nomi per non usare reserved words di javascript
+const numberOne = 3;
+const stringOne = "hello";
+const boolOne = false;
+console.log(numberOne);
+console.log(stringOne);
+console.log(boolOne);
 
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
@@ -14,10 +26,10 @@ const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; // NON MODIFICARE QUESTA RIGA
 const firstName = 'Jane'; // NON MODIFICARE QUESTA RIGA
 const lastName = 'Smith'; // NON MODIFICARE QUESTA RIGA
 
-const fullName = null;
+const fullName = `${firstName} ${lastName}`;
 console.log(fullName); // Output: "Jane Smith"
 
 // 4. Imposta questa variabile utilizzando una proprietà
 // sulla variabile alphabet per ottenere la sua lunghezza
-const numberOfLettersInAlphabet = null;
+const numberOfLettersInAlphabet = alphabet.length;
 console.log(numberOfLettersInAlphabet); // Output: 26
